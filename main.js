a9039a6c33541fcd74c27a59569adadce9b5b990
cheran <cherankumar.548@gmail.com>
2022-04-08 22:56:54 +0530

    added nodemon dev

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env.NODE_ENV = "development";
+
+// isDev is true if we are in development false if production.
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
 let mainWindow;
 
 function createMainWindow() {
@@ -12,14 +18,27 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev,
   });
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   // or
   // mainWindow.loadFile('./app/index.html');
-
   // using 2nd method
   mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createMainWindow);
+
+// on macOs stuff
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
