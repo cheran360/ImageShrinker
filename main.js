099911f45fe317a03fc74ed430f2501f59916bba
cheran <cherankumar.548@gmail.com>
2022-04-09 15:47:42 +0530

    completed upto Menu and Global Shortcuts

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 
 process.env.NODE_ENV = "development";
 
@@ -19,6 +19,7 @@ function createMainWindow() {
     height: 600,
     icon: "./assets/icons/Icon_256x256.png",
     resizable: isDev,
+    backgroundColor: "white",
   });
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -28,7 +29,39 @@ function createMainWindow() {
   mainWindow.loadFile("./app/index.html");
 }
 
-app.on("ready", createMainWindow);
+app.on("ready", () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () => {
+    mainWindow.toggleDevTools();
+  });
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+// Spread operator to setApplicationMenu in mac as -
+// - menu is slightly different from windows to macOs
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        // accelerator: isMac ? "Command+W" : "Ctrl+W",
+        // or
+        accelerator: "CmdOrCtrl+W",
+        click: () => app.quit(),
+      },
+    ],
+  },
+];
 
 // on macOs stuff
 app.on("window-all-closed", () => {
