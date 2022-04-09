7f37a0f2719360a37e3eb48b4abe6fa8b2f62194
cheran <cherankumar.548@gmail.com>
2022-04-09 16:19:24 +0530

    corrected code and menu completed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 process.env.NODE_ENV = "development";
 
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== "production" ? true : false;
 const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -29,16 +30,30 @@ function createMainWindow() {
   mainWindow.loadFile("./app/index.html");
 }
 
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    title: "About ImageShrink",
+    width: 300,
+    height: 300,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: false,
+    backgroundColor: "white",
+  });
+
+  aboutWindow.loadFile("./app/about.html");
+}
+
 app.on("ready", () => {
   createMainWindow();
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
-  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
-  globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () => {
-    mainWindow.toggleDevTools();
-  });
+  // global shortcuts removed
 
   mainWindow.on("closed", () => {
     mainWindow = null;
@@ -48,19 +63,48 @@ app.on("ready", () => {
 // Spread operator to setApplicationMenu in mac as -
 // - menu is slightly different from windows to macOs
 const menu = [
-  ...(isMac ? [{ role: "appMenu" }] : []),
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
   {
-    label: "File",
-    submenu: [
-      {
-        label: "Quit",
-        // accelerator: isMac ? "Command+W" : "Ctrl+W",
-        // or
-        accelerator: "CmdOrCtrl+W",
-        click: () => app.quit(),
-      },
-    ],
+    role: "fileMenu",
   },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
 ];
 
 // on macOs stuff
