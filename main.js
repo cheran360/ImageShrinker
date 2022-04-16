9b9d4b6299352537b0744a20fc073adbae6be78b
cheran <cherankumar.548@gmail.com>
2022-04-16 21:35:20 +0530

    ipcMain added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 
 process.env.NODE_ENV = "development";
 
@@ -111,6 +111,9 @@ const menu = [
     : []),
 ];
 
+ipcMain.on("image:minimize", (e, options) => {
+  console.log(options);
+});
 // on macOs stuff
 app.on("window-all-closed", () => {
   if (!isMac) {
